c682355ea3259763eb73fc624c8fe0e63131a3ec
RogerioR3 <117490600+RogerioR3@users.noreply.github.com>
2024-07-18 12:59:25 -0300

    Update Produto.java
    Mudança estética em Produto.java

--- Produto.java
+++ Produto.java
@@ -1,36 +1,77 @@
 public class Produto {
-    String Nome;
-    double Preco;
-    int Estoque;
-    int Qnt;
-    int Codigo;
-    
-    Produto (int Codigo, String Nome, double Preco, int Estoque, int Qnt) {
-        this.Nome = Nome;
-        this.Preco = Preco;
-        this.Estoque = Estoque;
-        this.Qnt = Qnt;
-        this.Codigo = Codigo;
+    private String nome;
+    private double preco;
+    private int estoque;
+    private int qnt;
+    private int codigo;
+
+    public Produto(int codigo, String nome, double preco, int estoque, int qnt) {
+        this.nome = nome;
+        this.preco = preco;
+        this.estoque = estoque;
+        this.qnt = qnt;
+        this.codigo = codigo;
     }
-    
-    int comprarProduto(int x){
-        x = x + Qnt;
+
+    public int comprarProduto(int x) {
+        x = x + qnt;
         return x;
     }
-    
-    int alterarQnt(int n){
-        if(n>0 && n<=Estoque){
+
+    public int alterarQnt(int n) {
+        if (n > 0 && n <= estoque) {
             return n;
-        }else{
+        } else {
             return 0;
         }
     }
-    
-    String imprimeDados () {
-        return Codigo + ". " + "    Nome: " + Nome + "    Preco: " + Preco + " reais    Estoque: " + Estoque + "\n";
+
+    public String imprimeDados() {
+        return codigo + ". " + "  |  Nome: " + nome + "  |  Preco: R$" + preco + " reais  |  Estoque: " + estoque + "\n";
     }
-    
-    String carrinhoDados () {
-        return "Codigo do Produto: "+ Codigo + "    Nome: " + Nome + "    Preco: " + Preco*Qnt + " reais    Quantidade: " + Qnt + "\n";
+
+    public String carrinhoDados() {
+        return "Codigo do Produto: " + codigo + "  |  Nome: " + nome + "  |  Preco: R$" + preco * qnt + " reais  |  Quantidade: " + qnt + "\n";
+    }
+
+    // Getters and Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+
+    public int getQnt() {
+        return qnt;
+    }
+
+    public void setQnt(int qnt) {
+        this.qnt = qnt;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
     }
 }
